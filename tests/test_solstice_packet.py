d315bfc1cb284e138402d504f394fa1239bd7f62
Fernando Mano <fermano@gmail.com>
2026-09-19 23:21:13 -0300

    Cover Solstice compatibility hint

--- tests/test_solstice_packet.py
+++ tests/test_solstice_packet.py
@@ -12,3 +12,11 @@ def test_solstice_packet_uses_policy_default_when_region_is_blank():
     policy = RegionPolicy(("us-east-1", "eu-west-1"), "euw1")
 
     assert resolve_solstice_packet_region(policy, {"region": " "}) == "eu-west-1"
+
+
+def test_solstice_packet_accepts_compatibility_region_hint():
+    policy = RegionPolicy(("us-east-1", "eu-west-1"), "us-east-1")
+
+    assert resolve_solstice_packet_region(
+        policy, {"region": "", "region_hint": "euw1"}
+    ) == "eu-west-1"
